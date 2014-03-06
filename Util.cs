c1fa290bc004f66225883d09520408fd9d0092d4
Drew Rathbone <drew@rathers.plus.com>
2014-03-06 20:04:57 +0000

    Util.cs, lib\Goose.lua: Utility methods

---
+++ Util.cs
@@ -0,0 +1,47 @@
+﻿using NLua;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goose
+{
+    public static class Util
+    {
+        public static object[] ObjArrayFromTable(LuaTable t)
+        {
+            object[] r = new object[t.Values.Count];
+            int i = 0;
+            foreach (object value in t.Values)
+            {
+                if (value is LuaTable) { r[i] = ObjFromTable((LuaTable)value); }
+                else
+                {
+                    r[i] = value;
+                }
+                i++;
+            }
+            return r;
+        }
+
+        public static IEnumerable<string> StringArrayFromTable(LuaTable t)
+        {
+            return ObjArrayFromTable(t).Cast<string>().ToArray();
+        }
+
+        public static object ObjFromTable(LuaTable t)
+        {
+            Dictionary<string, object> args = new Dictionary<string, object>();
+
+            foreach (DictionaryEntry item in t)
+            {
+                object obj;
+                if (item.Key is double) { return ObjArrayFromTable(t); }
+                if (item.Value is LuaTable) { obj = ObjFromTable((LuaTable)item.Value); } else { obj = item.Value; }
+                args.Add(item.Key.ToString(), obj);
+            }
+            return args;
+        }
+    }
+}
---
+++ lib/Goose.lua
@@ -0,0 +1,20 @@
+﻿
+function arr(t,...)
+arg = table.pack(...)
+if arg.n>0 then arg.n = nil table.insert(arg,1,t) t = arg 
+elseif type(t) ~= "table" and type(t) ~= "userdata" then t = {t}
+end
+return Utility.ObjArrayFromTable(t)
+end
+
+function obj(t)
+return Utility.ObjFromTable(t)
+end
+
+function str_arr(t,...)
+arg = table.pack(...)
+if arg.n>0 then arg.n = nil table.insert(arg,1,t) t = arg 
+elseif type(t) ~= "table" and type(t) ~= "userdata" then t = {t}
+end
+return Utility.StringArrayFromTable(t)
+end
