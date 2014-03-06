a497538b7075e68fb9fa987c4561eed7bf5b50bd
Drew Rathbone <drew@rathers.plus.com>
2014-03-06 20:55:40 +0000

    Added GooseTest: VERY small test project

---
+++ GooseTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using Goose;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GooseTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VM vm = new VM();
+            vm.Compile("GooseFeatures", new string[]{
+@"
+using System;
+namespace GooseInYourFaceWhatchaGonnaDo 
+{
+        public class MotherGoose 
+        { 
+            public MotherGoose()
+            {
+                Console.WriteLine(""Moo!"");
+            }
+        }    
+}" 
+            });
+
+            vm.DoString("MotherGoose()");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
